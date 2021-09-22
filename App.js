5a06566c70448aa439ac253990dbefa1551d4104
mdumar112233 <mdumarfaruk187326@gmail.com>
2021-09-22 20:03:41 +0600

    update  camera

--- App.js
+++ App.js
@@ -3,26 +3,35 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import 'react-native-gesture-handler';
 import { RNCamera } from 'react-native-camera';
 
-const Camera = () => {
+ const Camera = () => {
   let cameraRef = useRef(null);
   const [camType,  setCamType] = useState(RNCamera.Constants.Type.back);
   const [flash, setFlash] = useState(RNCamera.Constants.FlashMode.off);
 
-    takePicture = async () => {
+   const  takePicture = async () => {
       if (cameraRef) {
         const options = { quality: 0.5, base64: true };
-        const data = await cameraRef.takePictureAsync(options);
+        const data = await cameraRef.current.takePictureAsync(options);
         console.log(data.uri);
       }
     };
 
     const flipCamera = () => {
       if(camType === RNCamera.Constants.Type.back){
-        setCamType(RNCamera.Constants.Type.on)
+        setCamType(RNCamera.Constants.Type.front)
       } else {
-        setCamType(RNCamera.Constants.Type.off)
+        setCamType(RNCamera.Constants.Type.back)
       }
     }
+
+    const toggleFlash = () => {
+      if(flash === RNCamera.Constants.FlashMode.off){
+        setFlash(RNCamera.Constants.FlashMode.on)
+      }else{
+        setFlash(RNCamera.Constants.FlashMode.off)
+      }
+    }
+
     return (
       <View style={styles.container}>
         <RNCamera
@@ -36,24 +45,24 @@ const Camera = () => {
             buttonPositive: 'Ok',
             buttonNegative: 'Cancel',
           }}
-          androidRecordAudioPermissionOptions={{
-            title: 'Permission to use audio recording',
-            message: 'We need your permission to use your audio',
-            buttonPositive: 'Ok',
-            buttonNegative: 'Cancel',
-          }}
-          onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
-          }}
+          // androidRecordAudioPermissionOptions={{
+          //   title: 'Permission to use audio recording',
+          //   message: 'We need your permission to use your audio',
+          //   buttonPositive: 'Ok',
+          //   buttonNegative: 'Cancel',
+          // }}
+          // onGoogleVisionBarcodesDetected={({ barcodes }) => {
+          //   console.log(barcodes);
+          // }}
         />
         <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity onPress={() => takePicture()} style={styles.capture}>
+          <TouchableOpacity onPress={() => toggleFlash()} style={styles.capture}>
             <Text style={{ fontSize: 14 }}> flash </Text>
           </TouchableOpacity>
           <TouchableOpacity onPress={() => takePicture()} style={styles.capture}>
             <Text style={{ fontSize: 14 }}> SNAP </Text>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => takePicture()} style={styles.capture}>
+          <TouchableOpacity onPress={() => flipCamera()} style={styles.capture}>
             <Text style={{ fontSize: 14 }}> type </Text>
           </TouchableOpacity>
         </View>
@@ -83,11 +92,12 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+// export default function App() {
+//   return (
+//     <View>
+//       <Text>App</Text>
+//     </View>
+//   )
+// }
 
+export default Camera;
